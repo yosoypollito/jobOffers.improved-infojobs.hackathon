42df94bfb0f829564dbe90470a3cfd1dc9e897d3
Daniel Iglesias <dev.daif@gmail.com>
2023-05-27 04:39:48 -0400

    style: added cursor pointer

--- app/components/Radio.tsx
+++ app/components/Radio.tsx
@@ -2,7 +2,7 @@ import { HTMLProps } from "react";
 
 export default function Radio(props: HTMLProps<HTMLInputElement>) {
   return (
-    <label className="flex flex-row items-center gap-1 pointer text-sm text-left">
+    <label className="flex flex-row items-center gap-1 pointer text-sm text-left cursor-pointer">
       <div className="min-w-[16px] min-h-[16px] w-4 h-4 border-2 border-gray rounded-sm">
       </div>
       <input className="hidden" type="checkbox" {...props} />
