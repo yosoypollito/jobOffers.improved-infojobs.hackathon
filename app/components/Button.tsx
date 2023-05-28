d39176ca0b70bde8df853ae28f588210502fe286
Daniel Iglesias <dev.daif@gmail.com>
2023-05-28 08:46:17 -0400

    feat: allow button handle different classNames

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,7 +3,10 @@ import { HTMLAttributes } from "react";
 export default function Button(props: HTMLAttributes<HTMLButtonElement>) {
 
   return (
-    <button className="text-xs flex flex-row gap-2 items-center justify-center bg-white text-primary w-fit p-2 rounded uppercase font-semibold shadow-focus" {...props}>
+    <button {...{
+      ...props,
+      className: "text-xs flex flex-row gap-2 items-center justify-center bg-white text-primary w-fit p-2 rounded uppercase font-semibold shadow-focus" + props.className
+    }}>
       {props.children}
     </button>
   )
