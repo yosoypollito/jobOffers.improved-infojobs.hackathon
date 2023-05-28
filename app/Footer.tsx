26f93e580fb4d235bfda4630e4466df630df1cb5
Daniel Iglesias <dev.daif@gmail.com>
2023-05-28 11:05:14 -0400

    feat: added small footer text to mobile

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -14,7 +14,7 @@ const FooterLink = (props: HTMLProps<HTMLAnchorElement>) => {
 export default function Footer() {
 
   return (
-    <footer className="flex flex-row justify-center p-4 bg-ij-container-bg border-ij-container-border border-t-2 text-gray">
+    <footer className="flex flex-row justify-center p-4 bg-ij-container-bg border-ij-container-border border-t-2 text-gray text-sm sm:text-base">
       <p>
         Creado con <FooterLink href="https://www.infojobs.net/" target="_blank">InfoJobs</FooterLink> y <FooterLink href="https://openai.com/" target="_blank">OpenAi</FooterLink> por <FooterLink href="https://github.com/yosoypollito" target="_blank">Daif</FooterLink>
       </p>
