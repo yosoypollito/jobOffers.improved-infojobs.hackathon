682ea01c2a96919926533b296f772550f1eadb5a
Daniel Iglesias <dev.daif@gmail.com>
2023-05-28 08:58:32 -0400

    feat: now sizes are optional

--- app/components/LoadingSpin.tsx
+++ app/components/LoadingSpin.tsx
@@ -1,4 +1,4 @@
-export default function LoadingSpin({ sizeClassNames }: { sizeClassNames: string }) {
+export default function LoadingSpin({ sizeClassNames }: { sizeClassNames?: string }) {
   return (
     <div className={`animate-spin border-2 border-primary border-t-accent rounded-full ${sizeClassNames || "w-10 h-10"}`}></div>
   )
