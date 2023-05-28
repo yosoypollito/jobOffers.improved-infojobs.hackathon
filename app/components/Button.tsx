14b3aff336dabe547da0332d800357c58eace077
Daniel Iglesias <dev.daif@gmail.com>
2023-05-28 08:52:26 -0400

    fix: now button handle correctly extra classnames

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,7 +5,7 @@ export default function Button(props: HTMLAttributes<HTMLButtonElement>) {
   return (
     <button {...{
       ...props,
-      className: "text-xs flex flex-row gap-2 items-center justify-center bg-white text-primary w-fit p-2 rounded uppercase font-semibold shadow-focus" + props.className
+      className: `text-xs flex flex-row gap-2 items-center justify-center bg-white text-primary w-fit p-2 rounded uppercase font-semibold shadow-focus ${props.className}`
     }}>
       {props.children}
     </button>
