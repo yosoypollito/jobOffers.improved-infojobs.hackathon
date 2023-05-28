dcedc553d28a9b2f73d419870f5a2f23fa78927b
Daniel Iglesias <dev.daif@gmail.com>
2023-05-28 09:53:08 -0400

    style: changed text for close filters

--- app/components/Filters.tsx
+++ app/components/Filters.tsx
@@ -11,7 +11,7 @@ export default function Filters({ facets }: { facets: Facets }) {
   return (
     <div className={`fixed md:flex flex-col bg-white z-10 w-full min-w-[220px] row-span-1 rounded-md overflow-auto max-h-screen md:relative md:overflow-hidden top-0 left-0 md:max-h-full transition-all duration-300 md:translate-y-0 ${showFilters ? 'flex translate-y-0' : 'translate-y-full'}`}>
       <div className="p-4 border-b-2 border-b-ij-container-border w-full md:hidden">
-        <Button onClick={toggleShowFilters}>Cerrar</Button>
+        <Button onClick={toggleShowFilters}>Mostrar ofertas</Button>
       </div>
       <div className="grid grid-cols-[1fr] p-4 w-full gap-10">
         {listOfFacets.length > 0 ? listOfFacets.map((facet) => {
