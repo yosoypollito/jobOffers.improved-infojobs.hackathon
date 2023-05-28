885ba1d6e461f53b833f36eacec96ac8228f2039
Daniel Iglesias <dev.daif@gmail.com>
2023-05-28 12:04:22 -0400

    fix: deleting min errors

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -41,7 +41,7 @@ export default function NavBar() {
 
         <div className="grid grid-cols-3 md:flex md:flex-row gap-2 w-full md:w-fit">
           <button onClick={toggle} className="md:hidden text-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" stroke-width={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
               <path d="M4 6l16 0"></path>
               <path d="M4 12l16 0"></path>
--- app/components/CheckBox.tsx
+++ app/components/CheckBox.tsx
@@ -5,7 +5,7 @@ export default function CheckBox(props: HTMLProps<HTMLInputElement>) {
     <label className="flex flex-row items-center gap-2 pointer text-sm text-left cursor-pointer">
       <div className={`relative flex items-center p-[3px] justify-center min-w-[16px] min-h-[16px] w-4 h-4 border-2 ${props.checked ? "border-primary bg-primary" : "border-gray"} rounded-sm`}>
         <svg xmlns="http://www.w3.org/2000/svg"
-          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${props.checked ? "text-white" : "text-gray"}`} width={10} height={10} viewBox="0 0 24 24" stroke-width={4} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${props.checked ? "text-white" : "text-gray"}`} width={10} height={10} viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
           <path d="M5 12l5 5l10 -10"></path>
         </svg>
