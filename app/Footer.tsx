56fc8ea2f3cc58334d0cc7e93069f30a3dc23032
Daniel Iglesias <dev.daif@gmail.com>
2023-05-30 07:28:39 -0400

    style: change footer text

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-row justify-center p-4 bg-ij-container-bg border-ij-container-border border-t-2 text-gray text-sm sm:text-base">
       <p>
-        Creado con <FooterLink href="https://www.infojobs.net/" target="_blank">InfoJobs</FooterLink> y <FooterLink href="https://openai.com/" target="_blank">OpenAi</FooterLink> por <FooterLink href="https://github.com/yosoypollito" target="_blank">Daif</FooterLink>
+        Creado por <FooterLink href="https://github.com/yosoypollito" target="_blank">Daif</FooterLink> para la hackathon de <FooterLink href="https://www.youtube.com/midudev" target="_blank">Midudev</FooterLink> junto <FooterLink href="https://www.infojobs.net/" target="_blank">InfoJobs</FooterLink>
       </p>
     </footer>
   )
